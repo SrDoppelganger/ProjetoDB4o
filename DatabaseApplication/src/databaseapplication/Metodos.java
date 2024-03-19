eb86419a98711c09d4de6961c086f32c49c54c34
Sr.Doppelgänger <Arthur.albuquerque2004@gmail.com>
2024-03-19 09:19:25 -0300

    add try...catch para retificar erros comuns

--- DatabaseApplication/src/databaseapplication/Metodos.java
+++ DatabaseApplication/src/databaseapplication/Metodos.java
@@ -8,53 +8,66 @@ import java.util.List;
 
 public class Metodos {
     public static void adicionar(ObjectContainer container, String addNome, String addRaça){
-        Dados d = new Dados(addNome,addRaça);
-        container.store(d);
-        System.out.println("Add " + d);
+        try{
+            Dados d = new Dados(addNome,addRaça);
+            container.store(d);
+            System.out.println("Add " + d);
+        }catch(Exception e){
+            System.out.println("Dado(s) inválido(s)");
+        }
     }
     
     public static void deletar(ObjectContainer container, String delDado){
-        List<Dados> dados = container.query(new Predicate<Dados>(){
-        @Override
-        public boolean match(Dados o){
-        return o.getNome().equals(delDado);
-            }
-        });
-        
-        Dados d = dados.get(0);
-        container.delete(d);
-        
+        try{
+            List<Dados> dados = container.query(new Predicate<Dados>(){
+            @Override
+            public boolean match(Dados o){
+            return o.getNome().equals(delDado);
+                }
+            });
+
+            Dados d = dados.get(0);
+            container.delete(d);
+
         System.out.println(d.getNome()+" foi deletado com sucesso.");
+        }catch(Exception e){
+            System.out.println("Dado(s) não encontrado(s)");
+        }
+        
     }
     
     public static void buscar(ObjectContainer container, String seaDado){
-        List<Dados> dados = container.query(new Predicate<Dados>(){
-        @Override
-        public boolean match(Dados o){
+        
+            List<Dados> dados = container.query(new Predicate<Dados>(){
+            @Override
+            public boolean match(Dados o){
             return o.getNome().equals(seaDado);
-        }
-        }); 
-        for (Dados d : dados){
-            System.out.println("Nome|Raça");
-            System.out.println(d.getNome()+"|"+d.getRaça());
-        }
-        
-        
+            }
+            }); 
+            for (Dados d : dados){
+                System.out.println("Nome|Raça");
+                System.out.println(d.getNome()+"|"+d.getRaça());
+            }
     }
     
     public static void atualizar(ObjectContainer container, String oldNome, String newNome){
-        List<Dados> dados = container.query(new Predicate<Dados>(){
-        @Override
-        public boolean match(Dados o){
-        return o.getNome().equals(oldNome);
+        try{
+             List<Dados> dados = container.query(new Predicate<Dados>(){
+            @Override
+            public boolean match(Dados o){
+            return o.getNome().equals(oldNome);
+            }
+            });
+
+            Dados d = dados.get(0);
+            d.setNome(newNome);
+            container.store(d);
+        
+            System.out.println("Dado(s) atualizado(s) com sucesso.");
+        }catch(Exception e){
+            System.out.println("Dado(s) Inválido(s) ou Não Encontrado(s)");
         }
-        });
-        
-        Dados d = dados.get(0);
-        d.setNome(newNome);
-        container.store(d);
-        
-        System.out.println("Dado(s) atualizado(s) com sucesso.");
+       
     }
     
     public static void imprimir(ObjectContainer container){
